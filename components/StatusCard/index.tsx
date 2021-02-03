3a54ce5b75b7c4ead42864749421748da1852922
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-03 18:35:22 +0100

    feat: status card

---
+++ components/StatusCard/index.tsx
@@ -0,0 +1,47 @@
+import TimeAgo from "react-timeago";
+
+import styles from "./StatusCard.module.scss";
+
+interface StatusCardProps {
+    title: string;
+    status: number;
+    lastUpdated: Date;
+    block: string;
+}
+
+const StatusCard = ({
+    title,
+    status,
+    lastUpdated,
+    block,
+}: StatusCardProps): JSX.Element => {
+    return (
+        <div className={styles.relayerCard}>
+            <div className={styles.head}>
+                <span>{title}</span>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.left}>
+                    <p>
+                        Status: <span>{status}</span>
+                    </p>
+                    {lastUpdated && (
+                        <small>
+                            Updated:{" "}
+                            <TimeAgo
+                                date={new Date(new Date().getTime() - 1000)} // weird fix, but it works
+                            />
+                        </small>
+                    )}
+                </div>
+                <div className={styles.right}>
+                    <p>
+                        Lastest Block: <span>{block}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatusCard;
