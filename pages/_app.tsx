d69044cc2e530b92a0f79dd3a246eca437c80709
dylangolow <dylangolow@gmail.com>
2022-11-18 16:14:37 -0500

    chore: update _app.tsx for next 11.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import type { AppProps } from "next/app";
 import "../styles/global.scss";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
